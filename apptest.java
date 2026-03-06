9bf8e62ac5ffa83d942c467b6d091f65dd42fca7
achindhyakainoth <achindhyakainoth@gmail.com>
2026-03-06 16:44:55 +0530

    Create apptest.java

---
+++ apptest.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Jenkins Java Test Successful!");
+        
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+
+        if (sum == 15) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+        }
+    }
+}
