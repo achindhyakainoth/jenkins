72f47099996447a79669b12ac124812d67f3db5f
achindhyakainoth <achindhyakainoth@gmail.com>
2026-03-06 16:51:06 +0530

    Update AppTest.java

--- apptest.java
+++ apptest.java
@@ -1,15 +1,10 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Jenkins Java Test Successful!");
-        
+        System.out.println("Hello from Jenkins Maven Pipeline!");
+
         int a = 5;
         int b = 10;
-        int sum = a + b;
 
-        if (sum == 15) {
-            System.out.println("Test Passed");
-        } else {
-            System.out.println("Test Failed");
-        }
+        System.out.println("Sum = " + (a + b));
     }
 }
